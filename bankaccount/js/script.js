b46e2d961320a8d2089b63c9ef1846ae68b08192
Hugo <006.tran.h@gmail.com>
2023-05-31 23:25:45 -0700

    Add files via upload

---
+++ bankaccount/js/script.js
@@ -0,0 +1,33 @@
+// var nameBtn = document.getElementById("button-name");
+// var depositBtn = document.getElementById("button-deposit");
+// var withdrawlBtn = document.getElementById("button-withdrawal");
+
+// nameBtn.addEventListener("click", changeName);
+// depositBtn.addEventListener("click", depositMoney);
+// withdrawlBtn.addEventListener("click", withdrawalMoney);
+
+// var bankBalance = 0;
+
+// function changeName(){
+//     let userName = prompt("Enter your name");
+//     document.getElementById("bankInfo-1").innerHTML = `Name: ${userName}`;
+// }
+
+// function depositMoney(){
+//     var depositValue = prompt("Enter a deposit value");
+//     bankBalance += +depositValue;
+//     document.getElementById("bankInfo-2").innerHTML = `Current balance: $${bankBalance}`;
+// }
+
+// function withdrawalMoney(){
+//     // function depositMoney(){
+//     var withdrawalValue = prompt("Enter a deposit value");
+//     bankBalance -= +withdrawalValue;
+//     document.getElementById("bankInfo-2").innerHTML = `Current balance: $${bankBalance}`;
+// }
+
+
+function bankAccount (ownerName){
+    let balance;
+    let owner = ownerName;
+}
