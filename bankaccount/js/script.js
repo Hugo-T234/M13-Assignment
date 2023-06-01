0672d46be0fa11a30e2d87951ba4e1be10203310
Hugo <006.tran.h@gmail.com>
2023-06-01 01:37:39 -0700

    Update script.js

--- bankaccount/js/script.js
+++ bankaccount/js/script.js
@@ -26,8 +26,54 @@
 //     document.getElementById("bankInfo-2").innerHTML = `Current balance: $${bankBalance}`;
 // }
 
-
 function bankAccount (ownerName){
-    let balance;
+    let balance = 0;
     let owner = ownerName;
+
+    return {
+        withdrawal: function(withdrawalAmount) {
+            if (withdrawalAmount > balance){
+                alert('Not enough balance in your account');
+            }
+            else balance -= +withdrawalAmount;
+        },
+        deposit: function(depositAmount) {
+            if (balance < 0){
+                alert('Invalid deposit number');
+            }
+            else balance += +depositAmount;
+        },
+        getBalance: function() {
+            return balance;
+        },
+        getOwnerName: function() {
+            return owner;
+        }
+    }
 }
+
+var nameBtn = document.getElementById("button-name");
+var depositBtn = document.getElementById("button-deposit");
+var withdrawlBtn = document.getElementById("button-withdrawal");
+
+let account = new bankAccount;
+
+nameBtn.addEventListener("click", function(){
+    let userName = prompt("Enter your name");
+    account = bankAccount(userName);
+    document.getElementById("bankInfo-1").innerHTML = `Name: ${userName}`;
+});
+// depositBtn.addEventListener("click", depositMoney);
+depositBtn.addEventListener("click", function(){
+    var depositValue = prompt("Enter a deposit value");
+    account.deposit(depositValue);
+    let bal = account.getBalance();
+    document.getElementById("bankInfo-2").innerHTML = `Balance: $${bal}`;
+});
+// withdrawlBtn.addEventListener("click", withdrawalMoney);
+withdrawlBtn.addEventListener("click", function(){
+    var withdrawalValue = prompt("Enter a deposit value");
+    account.withdrawal(withdrawalValue);
+    let bal = account.getBalance();
+    document.getElementById("bankInfo-2").innerHTML = `Balance: $${bal}`;
+});
